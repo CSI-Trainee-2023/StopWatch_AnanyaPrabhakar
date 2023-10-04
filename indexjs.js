55f2e7a3d8c562c5a44537347f95c036841c6665
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2023-10-04 19:13:17 +0530

    files added

---
+++ indexjs.js
@@ -0,0 +1,9 @@
+let startButton = document.getElementsByClassName('start');
+let resetButton = document.getElementsByClassName('reset');
+
+function buttonChange(){
+  const button = document.getElementByClassName('start').innerHTML="STOP";
+}
+
+
+document.getElementsByClassName('abc').innerHTML="abc";
