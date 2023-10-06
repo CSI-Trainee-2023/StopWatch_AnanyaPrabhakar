422095c85e44257bb753038e70b69b2311b1fc97
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2023-10-06 17:13:03 +0530

    added functinalities to both the buttons

--- indexjs.js
+++ indexjs.js
@@ -1,140 +1,124 @@
-
-
-
-var startBtn = true;
-var stopbutton = false;
 // taken variables for BUTTONS
 
-let startButton = document.getElementById('start');
-let resetButton = document.getElementById('reset');
+let startButton = document.getElementById("start");
+let resetButton = document.getElementById("reset");
 
 // taken variables for UNITS
 
 let counter = document.getElementById("count");
-let second = document.getElementById('second');
-let minutes = document.getElementById('minutes');
-let hours = document.getElementById('hour');
-
+let second = document.getElementById("second");
+let minutes = document.getElementById("minutes");
+let hours = document.getElementById("hour");
 
 // initializing variables
 
-let[hour,min,sec,count,timer] = [0,0,0,0,false];
- 
+let [hour, min, sec, count, timer, startbtn, stopbtn] = [0, 0, 0, 0, false, true, false];
+
+// Calling Functions of button
+
+startButton.addEventListener("click", start);
+resetButton.addEventListener("click", reset);
 
 
-function start(){
-     timer = true;
-     
-     stopWatch();
-}
-function reset(){
+
+startButton.addEventListener("click", function(){
+  if (startbtn == true){
+    start();
+  }
+  else if (stopbtn == true){
+    stop();
+  }
+});
+// Reset Function
+
+function reset() {
   timer = false;
-
-  hour = 0;
-  min = 0;
-  sec = 0;
-  count = 0;
-
-
+  [hour, min, sec, count] = [0, 0, 0, 0];
   counter.innerHTML = "00";
   second.innerHTML = "00:";
   minutes.innerHTML = "00:";
   hours.innerHTML = "00:";
 }
 
-function stop(){
-  timer = false;
+//  Start Function
+
+function start() {
   
+  startButton.innerHTML = "STOP";
+  resetButton.innerHTML = "LAP";
+  timer = true;
+  stopbtn = true;
+  
+  stopWatch();
 }
 
+// Stop Function
 
-startButton.addEventListener("click", start);
+function stop() {
+  timer = false;
+  startbtn = true;
+  stopbtn = false;
+  startButton.innerHTML = "START";
+  resetButton.innerHTML = "RESET";
+}
 
 resetButton.addEventListener("click", reset);
 
 
 
-
-let changeButton1 = document.querySelector("#start");
-let changeButton2 = document.querySelector("#reset");
-
-changeButton1.addEventListener("click",change);
-
-
-function change(){
-  if(timer == true){
-    changeButton1.innerHTML = "STOP";
-    changeButton2.innerHTML = "LAP";
-    changeButton1.addEventListener("click",stop);
-  }
-  if(timer == false){
-    changeButton1.innerHTML = "START";
-    changeButton2.innerHTML = "RESET";
-    changeButton1.addEventListener("click",start);
-  }
-  
-}
+startButton.addEventListener("click", function change(){
+  startButton.addEventListener("click",stop);
+});
 
 
 
 
-  
+// StopWatch Function
 
-
-
-
-function stopWatch(){
-    if(timer){
-      count++;
-      if(count == 100){
-        count=0;
-        sec++;
-      }
-      if(sec == 60){
-          sec=0;
-          min++;
-      }
-      if(min == 60){
-          min=0;
-          hour++;
-      }
-
-      let [hr,secnd,mint,cont] = [hour,sec,min,count];
-      
-
-      if(hour<10){
-        hr = '0' + hr +':';
-      }
-      if(sec<10){
-        secnd = '0' + secnd +':';
-      }
-      if(min<10){
-        mint = '0' + mint +':';
-      }
-      if(count<10){
-        cont = '0' + cont;
-      }
-
-      if(hour>=10){
-        hr = hr +':';
-      }
-      if(sec>=10){
-        secnd = secnd +':';
-      }
-      if(min>=10){
-        mint = mint +':';
-      }
-      
-
-      
-      
-    
-      
-      
-      counter.innerHTML = cont;
-      second.innerHTML = secnd;
-      hours.innerHTML = hr;
-      minutes.innerHTML = mint;
-      setTimeout(stopWatch,10);
+function stopWatch() {
+  if (timer) {
+    count++;
+    if (count == 100) {
+      count = 0;
+      sec++;
     }
+    if (sec == 60) {
+      sec = 0;
+      min++;
+    }
+    if (min == 60) {
+      min = 0;
+      hour++;
+    }
+
+    let [hr, secnd, mint, cont] = [hour, sec, min, count];
+
+    if (hour < 10) {
+      hr = "0" + hr + ":";
+    }
+    if (sec < 10) {
+      secnd = "0" + secnd + ":";
+    }
+    if (min < 10) {
+      mint = "0" + mint + ":";
+    }
+    if (count < 10) {
+      cont = "0" + cont;
+    }
+
+    if (hour >= 10) {
+      hr = hr + ":";
+    }
+    if (sec >= 10) {
+      secnd = secnd + ":";
+    }
+    if (min >= 10) {
+      mint = mint + ":";
+    }
+    counter.innerHTML = cont;
+    second.innerHTML = secnd;
+    hours.innerHTML = hr;
+    minutes.innerHTML = mint;
+    setTimeout(stopWatch, 10);
+  }
 }
